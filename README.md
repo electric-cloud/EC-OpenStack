3f01f397f6f9f3293fa8b8fb935d67cb202e6dd1
Nikhil Vaze <nvaze@electric-cloud.com>
2015-02-10 18:27:21 -0800

    Implement waitForJob
    More refactoring to reduce hard coding
    Implement waitForJob
    Introduce COMMANDER_USER and COMMANDER_PASSWORD properties

--- README.md
+++ README.md
@@ -11,17 +11,19 @@ Run gradlew to compile the plugin
 
 ## Tests ##
 
-### Creating openstack-test.properties ###
+#### Creating openstack-test.properties ####
 Create an openstack-test.properties with the following content
 
-    OPENSTACK_USER=<user> 
+    COMMANDER_USER=<COMMANDER_USER>
+    COMMANDER_PASSWORD=<COMMANDER_PASSWORD>
+    OPENSTACK_USER=<USER> 
     OPENSTACK_PASSWORD=<PASSWORD>
     OPENSTACK_TENANTID=<ID>
     OPENSTACK_IDENTITY_URL=<URL>
     
 These represent secrets that **should not** be checked in.
 
-### Running tests ###
+#### Running tests ####
 Run the `test` task to run the system tests. You may want to specify the ElectricCommander Server to test against by way of the COMMANDER_SERVER environment variable.
 
 `COMMANDER_SERVER=192.168.158.20 ./gradlew test`
--- src/test/java/ecplugins/openstack/OpenStackProvisionTest.java
+++ src/test/java/ecplugins/openstack/OpenStackProvisionTest.java
@@ -2,6 +2,7 @@ package ecplugins.openstack;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -23,23 +24,26 @@ import static org.junit.Assert.assertNotNull;
 public class OpenStackProvisionTest {
 
     private static OSClient m_osClient;
-    private String COMMANDER_SERVER = System.getProperty("COMMANDER_SERVER");
-    private static String IDENTITY_URL = System.getProperty("OPENSTACK_IDENTITY_URL");
-    private static String USER = System.getProperty("OPENSTACK_USER");
-    private static String PASSWORD = System.getProperty("OPENSTACK_PASSWORD");
-    private static String TENANTID = System.getProperty("OPENSTACK_TENANTID");
+    private final String COMMANDER_SERVER = System.getProperty("COMMANDER_SERVER");
+    private final String COMMANDER_USER = System.getProperty("COMMANDER_USER");
+    private final String COMMANDER_PASSWORD = System.getProperty("COMMANDER_PASSWORD");
+    private final static String IDENTITY_URL = System.getProperty("OPENSTACK_IDENTITY_URL");
+    private final static String USER = System.getProperty("OPENSTACK_USER");
+    private final static String PASSWORD = System.getProperty("OPENSTACK_PASSWORD");
+    private final static String TENANTID = System.getProperty("OPENSTACK_TENANTID");
 
     @BeforeClass
-    public static void setup(){
+    public static void setup() {
 
         m_osClient = OSFactory.builder()
                 .endpoint(IDENTITY_URL)
-                .credentials(USER,PASSWORD)
+                .credentials(USER, PASSWORD)
                 .tenantId(TENANTID)
                 .authenticate();
     }
 
-    @Test public void testkeyPairCreation() {
+    @Test
+    public void testkeyPairCreation() {
 
 
         JSONObject param1 = new JSONObject();
@@ -52,9 +56,6 @@ public class OpenStackProvisionTest {
         try {
             jo.put("projectName", "EC-OpenStack-1.1.2");
             jo.put("procedureName", "CreateKeyPair");
-            jo.put("timeout", "600");
-            jo.put("pollInterval", "5");
-
 
             param1.put("value", "hp");
             param1.put("actualParameterName", "connection_config");
@@ -63,7 +64,7 @@ public class OpenStackProvisionTest {
             param2.put("value", "automatedTest-commandLine3");
 
             param3.put("actualParameterName", "tenant_id");
-            param3.put("value", "10200973354647");
+            param3.put("value", TENANTID);
 
             param4.put("actualParameterName", "tag");
             param4.put("value", "1");
@@ -80,7 +81,12 @@ public class OpenStackProvisionTest {
             e.printStackTrace();
         }
 
-        callRunProcedure(jo);
+        String jobId = callRunProcedure(jo);
+
+        String response = waitForJob(jobId);
+
+        // Check job status
+        assertEquals("Job completed without errors", "success", response);
 
         // Get the keypair from OpenStack
         Keypair keypair = m_osClient.compute().keypairs().get("automatedTest-commandLine3");
@@ -89,31 +95,100 @@ public class OpenStackProvisionTest {
         assertNotNull(keypair);
 
         // Grab the keypair name and check its name
-        assertEquals("Keypair name is","automatedTest-commandLine3",keypair.getName() );
+        assertEquals("Keypair name is set correctly", "automatedTest-commandLine3", keypair.getName());
 
     }
 
-    public void callRunProcedure(JSONObject jo) {
+    /**
+     * callRunProcedure
+     *
+     * @param jo
+     * @return the jobId of the job launched by runProcedure
+     */
+    public String callRunProcedure(JSONObject jo) {
 
         HttpClient httpClient = new DefaultHttpClient();
-
+        JSONObject result = null;
         try {
-            HttpPost httpPostRequest = new HttpPost("http://admin:changeme@"+ COMMANDER_SERVER + ":8000/rest/v1.0/jobs?request=runProcedure");
+            HttpPost httpPostRequest = new HttpPost("http://" + COMMANDER_USER
+                    + ":" + COMMANDER_PASSWORD + "@" + COMMANDER_SERVER
+                    + ":8000/rest/v1.0/jobs?request=runProcedure");
             StringEntity input = new StringEntity(jo.toString());
 
             input.setContentType("application/json");
             httpPostRequest.setEntity(input);
             HttpResponse httpResponse = httpClient.execute(httpPostRequest);
 
-            System.out.println("----------------------------------------");
-            System.out.println(httpResponse.getStatusLine());
-            System.out.println(EntityUtils.toString(httpResponse.getEntity()));
-            System.out.println("----------------------------------------");
+            result = new JSONObject(EntityUtils.toString(httpResponse.getEntity()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            httpClient.getConnectionManager().shutdown();
+        }
+
+        if (result != null) {
+            try {
+                return result.getString("jobId");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return "";
+
+
+    }
+
+    /**
+     * waitForJob: Waits for job to be completed and reports outcome
+     *
+     * @param jobId
+     * @return outcome of job
+     */
+    public String waitForJob(String jobId) {
+
+        String url = "http://" + COMMANDER_USER + ":" + COMMANDER_PASSWORD +
+                "@" + COMMANDER_SERVER + ":8000/rest/v1.0/jobs/" +
+                jobId + "?request=getJobStatus";
+        JSONObject jsonObject = performHTTPGet(url);
+
+        try {
+            while (jsonObject.getString("status").equalsIgnoreCase("completed")) {
+                jsonObject = performHTTPGet(url);
+            }
+
+            return jsonObject.getString("outcome");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+
+    }
+
+    /**
+     * Wrapper around a HTTP GET to a REST service
+     *
+     * @param url
+     * @return JSONObject
+     */
+    private JSONObject performHTTPGet(String url) {
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpResponse httpResponse = null;
+
+        try {
+            HttpGet httpGetRequest = new HttpGet(url);
+
+            httpResponse = httpClient.execute(httpGetRequest);
+            return new JSONObject(EntityUtils.toString(httpResponse.getEntity()));
 
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             httpClient.getConnectionManager().shutdown();
         }
+
+        return null;
     }
 }
